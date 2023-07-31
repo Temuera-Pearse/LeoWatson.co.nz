9f66f9d6d3353cf0209d0c1e966743cf61b361e6
Tem Pearse <tem.pearse01@gmail.com>
2023-07-31 15:47:36 +1200

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import { Suspense } from 'react'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
